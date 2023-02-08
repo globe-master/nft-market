968ab62cc8e528090ea54315420c337d83d5f14b
globe-master <globe.master.1215@outlook.com>
2023-02-08 12:45:20 +0100

    feat(stickers): improve bonus QR codes

--- stickers/1_qr_gen.py
+++ stickers/1_qr_gen.py
@@ -2,16 +2,25 @@
 
 import argparse
 import hashlib
+import math
 import os
 import qrcode
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 from typing import List
 from qrcode.image.pure import PymagingImage
 
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+colors = {
+    'red': '#EA033A',
+    'orange': '#FF5730',
+    'yellow': '#F5EA0A',
+    'green': '#56C553',
+    'blue': '#5C96FF',
+    'purple': '#8D52FF'
+}
 
 logo = Image.open('./logo.png')
+font = ImageFont.truetype('./font.ttf', 19)
 
 
 class Player:
@@ -33,16 +42,26 @@ def generate_players(players_count: int, salt: str, key_len: int, bonus: bool):
     return [generate_player(index, salt, key_len, bonus) for index in range(players_count)]
 
 
-def colorize(index: int) -> str:
-    return colors[index % len(colors)]
+def tell_color_name(index: int) -> str:
+    return list(colors.keys())[index % len(colors.keys())]
+
+
+def tell_color_code(index: int) -> str:
+    return colors[tell_color_name(index)]
+
+
+def decode_color_code(encoded: str) -> (int, int, int):
+    return int(encoded[1:3], 16), int(encoded[3:5], 16), int(encoded[5:7], 16)
 
 
 def generate_qr_code(player: Player, base_url: str, output_dir: str, bonus: bool):
     url = f'{base_url}{player.key}'
-    output_path = f'{output_dir}/{player.index}-{player.key}-{colorize(player.index)}.png'
+    color_name = tell_color_name(player.index)
+    interpolation = 'bonus-' if bonus else ''
+    output_path = f'{output_dir}/{interpolation}{player.index}-{player.key}-{color_name}.png'
 
     border = 4.5 if bonus else 0
-    box_size = 30 if bonus else 1
+    box_size = 30 if bonus else 10
 
     img = qrcode.make(url, image_factory=PymagingImage, error_correction=qrcode.ERROR_CORRECT_M, border=border,
                       box_size=box_size)
@@ -51,19 +70,35 @@ def generate_qr_code(player: Player, base_url: str, output_dir: str, bonus: bool
         img.save(file, 'PNG')
 
     if bonus:
-        im = Image.open(output_path)
+        qr = Image.open(output_path)
+
+        color_code = decode_color_code(tell_color_code(player.index))
+        serial = f'{player.key}{color_name[0].upper()}{player.index:03d}'
+
+        white = Image.new('RGB', (qr.width, qr.height), 'white')
+        solid = Image.new('RGB', (qr.width, qr.height), 'white')
+        ImageDraw.Draw(solid).rectangle([(0, 0), (solid.width, solid.height)], color_code)
+        background = Image.blend(white, solid, 0.25)
+
+        mask = qr.convert('L')
+        im = Image.composite(background, qr, mask)
 
         center = (im.width - logo.width) // 2
-        margin = 20
+        true_center = im.width // 2 - 15
+        margin = 25
 
-        logo_90 = logo.rotate(90, expand=1)
-        logo_180 = logo_90.rotate(90, expand=1)
-        logo_270 = logo_180.rotate(90, expand=1)
+        ImageDraw.Draw(im).rectangle([(true_center, true_center), (true_center + 29, true_center + 29)], color_code)
+        ImageDraw.Draw(im).text((130, im.width - 146), serial, 'white', font=font, spacing=-2)
 
-        im.paste(logo, (center, margin), logo)
-        im.paste(logo_90, (margin, center), logo_90)
-        im.paste(logo_180, (center, im.height - logo_180.height - margin), logo_180)
-        im.paste(logo_270, (im.width - logo_270.width - margin, center), logo_270)
+        logo_bottom = logo
+        logo_left = logo_bottom.rotate(90, expand=1)
+        logo_top = logo_bottom.rotate(180)
+        logo_right = logo_bottom.rotate(270, expand=1)
+
+        im.paste(logo_top, (center, margin), logo_top)
+        im.paste(logo_right, (margin, center), logo_right)
+        im.paste(logo_bottom, (center, im.height - logo_bottom.height - margin), logo_bottom)
+        im.paste(logo_left, (im.width - logo_left.width - margin, center), logo_left)
 
         im.save(output_path)
 
@@ -78,7 +113,7 @@ def generate_qr_codes(players: List[Player], base_url: str, output_dir: str, bon
 
 def main(config):
     players = generate_players(int(config.players), config.salt, int(config.key_len), config.bonus)
-    [print(f'{player.index} → {player.key} ({colorize(player.index)})') for player in players]
+    [print(f'{player.index};{player.key};{tell_color_name(player.index)}') for player in players]
     generate_qr_codes(players, config.base_url, config.output_dir, config.bonus)
 
 
