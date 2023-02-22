40ed78d9adb3ff746dace3fe21b5fe16bc723036
globe-master <globe.master.1215@outlook.com>
2023-02-22 08:59:58 +0100

    fix(ui/game-info): add name next to score

--- packages/ui/src/views/MainContent.vue
+++ packages/ui/src/views/MainContent.vue
@@ -4,14 +4,20 @@
       <InteractionInfo />
     </template>
     <template v-slot:main>
-      <div class="game-info" v-if="!player.showPalettePanel">
-        <ProviderConnected class="provider-info" />
-        <GameOverCountdown />
-        <BonusCountdown v-if="player.bonus" />
-        <GameInfo>
-          <p>{{ player.score }} Wpx</p></GameInfo
-        >
-      </div>
+      <transition name="fade">
+        <div class="game-info" v-if="!player.showPalettePanel">
+          <ProviderConnected class="provider-info" />
+          <GameOverCountdown />
+          <BonusCountdown v-if="player.bonus" />
+          <GameInfo>
+            <p>@{{ player.username }}</p></GameInfo
+          >
+          <GameInfo>
+            <p>{{ player.score }} WPX</p></GameInfo
+          >
+        </div>
+      </transition>
+      <MintInformation />
       <PixelBoard />
     </template>
     <template v-slot:bottom>
